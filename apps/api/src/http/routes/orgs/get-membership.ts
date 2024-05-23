df3852bbff0b4a37f4e79853120a084f670ebe8d
filliperomero <fillipe.romero@gmail.com>
2024-05-22 22:46:28 -0600

    Add get membership route

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,44 @@
+import { roleSchema } from '@mts/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/membership',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get user membership on organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({ slug: z.string() }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.string().uuid(),
+                role: roleSchema,
+                organizationId: z.string().uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const { membership } = await request.getUserMembership(slug)
+
+        return reply.status(201).send({
+          membership: {
+            id: membership.id,
+            role: membership.role,
+            organizationId: membership.organizationId,
+          },
+        })
+      },
+    )
+}
